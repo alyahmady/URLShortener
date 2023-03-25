939b8c7de2108ecce4ada90c3744cbf9eb757f75
alyahmady <better.aly.ahmady@gmail.com>
2023-03-25 18:35:54 +0430

    fix: update configurations of `settings.py`

--- URLShortener/settings/__init__.py
+++ URLShortener/settings/__init__.py
@@ -1,4 +1,5 @@
 from .apps import INSTALLED_APPS
+from .auth import AUTH_PASSWORD_VALIDATORS, AUTHENTICATION_BACKENDS
 from .base import (
     ALLOWED_HOSTS,
     WSGI_APPLICATION,
@@ -7,7 +8,6 @@ from .base import (
     ROOT_URLCONF,
     DEBUG,
     TEMPLATES,
-    AUTH_PASSWORD_VALIDATORS,
     CSRF_TRUSTED_ORIGINS,
 )
 from .logging import LOGS_DIR, CORE_LOG_FILE, ERROR_LOG_FILE, DEBUG_LOG_FILE
@@ -35,3 +35,5 @@ from .path import (
     MEDIA_URL,
     MEDIA_ROOT,
 )
+from .rest_framework import API_PREFIX, API_VERSION, REST_FRAMEWORK
+from .swagger import SPECTACULAR_SETTINGS
