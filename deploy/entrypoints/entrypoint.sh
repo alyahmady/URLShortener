b34191a82b93e2d741be0036f6d1a737a3cf0625
alyahmady <better.aly.ahmady@gmail.com>
2023-03-26 13:21:15 +0430

    fix: issues in EntryPoint and Redis env file

--- deploy/entrypoints/entrypoint.sh
+++ deploy/entrypoints/entrypoint.sh
@@ -5,8 +5,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-python3 manage.py initialize
 
 gunicorn URLShortener.asgi:application
